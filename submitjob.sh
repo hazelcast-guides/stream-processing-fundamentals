09b85b7ae8b3b895cd766402599753ba88964677
Randy May <randy.may@hazelcast.com>
2023-02-01 20:07:48 -0500

    update job script to give each deploy a unique job

--- submitjob.sh
+++ submitjob.sh
@@ -2,5 +2,5 @@
 docker compose exec cli hz-cli submit \
     -c=hazelcast.platform.solutions.machineshop.TemperatureMonitorPipeline \
     -t=dev@hz   \
-    -n=temperature-monitor \
+    -n=temp_monitor_`date +%H%M%S` \
     /project/monitoring-pipeline/target/monitoring-pipeline-1.0-SNAPSHOT.jar
