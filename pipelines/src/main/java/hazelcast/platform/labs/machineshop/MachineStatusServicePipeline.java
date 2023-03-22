38b654812c8b4ba663b2f74e0f3d99623975ce67
Randy May <randy.may@hazelcast.com>
2023-03-22 18:04:01 -0400

    Update the status service pipeline.

--- pipelines/src/main/java/hazelcast/platform/labs/machineshop/MachineStatusServicePipeline.java
+++ pipelines/src/main/java/hazelcast/platform/labs/machineshop/MachineStatusServicePipeline.java
@@ -3,28 +3,25 @@ package hazelcast.platform.labs.machineshop;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.jet.config.JobConfig;
-import com.hazelcast.jet.datamodel.KeyedWindowResult;
 import com.hazelcast.jet.datamodel.Tuple2;
-import com.hazelcast.jet.datamodel.Tuple3;
 import com.hazelcast.jet.datamodel.Tuple4;
+import com.hazelcast.jet.datamodel.Tuple5;
 import com.hazelcast.jet.pipeline.*;
-import com.hazelcast.map.impl.journal.MapEventJournal;
 import com.hazelcast.nio.serialization.ClassDefinition;
 import com.hazelcast.nio.serialization.genericrecord.GenericRecord;
 import com.hazelcast.nio.serialization.genericrecord.GenericRecordBuilder;
-import hazelcast.platform.labs.machineshop.domain.MachineStatusSummary;
 import hazelcast.platform.labs.machineshop.domain.Names;
+import hazelcast.platform.labs.machineshop.domain.StatusServiceResponse;
 
-import java.io.Serializable;
 import java.util.Map;
 
 public class MachineStatusServicePipeline {
 
-    private static String categorizeTemp(double temp, short warningLimit, short criticalLimit){
+    private static String categorizeTemp(short temp, short warningLimit, short criticalLimit){
         String result;
-        if (temp > (double) criticalLimit)
+        if (temp >  criticalLimit)
             result = "red";
-        else if (temp > (double) warningLimit)
+        else if (temp >  warningLimit)
             result = "orange";
         else
             result = "green";
@@ -62,37 +59,65 @@ public class MachineStatusServicePipeline {
 
         Pipeline pipeline = Pipeline.create();
 
-        // results is a Tuple3 (serialNum, warningTemp, criticalTemp)
-        StreamStage<Tuple3<String, Short, Short>> results =
+        // results is a Tuple4 (requestId, serialNum, warningTemp, criticalTemp)
+        StreamStage<Tuple4<String, String, Short, Short>> limits =
                 pipeline.readFrom(Sources.<String, String>mapJournal(
                         inputMapName, JournalInitialPosition.START_FROM_CURRENT))
                         .withoutTimestamps()
-                        .groupingKey( entry -> entry.getValue())
-                        .<GenericRecord,Tuple3<String,Short, Short>>mapUsingIMap(Names.PROFILE_MAP_NAME, (entry, gr) -> Tuple3.tuple3(
-                                entry.getValue(),
-                                gr.getInt16("warningTemp"),
-                                gr.getInt16("criticalTemp")
+                        .groupingKey(Map.Entry::getValue)
+                        .<GenericRecord,Tuple4<String, String,Short, Short>>mapUsingIMap(
+                                Names.PROFILE_MAP_NAME,
+                                (entry, gr) -> Tuple4.tuple4(
+                                        entry.getKey(),
+                                        entry.getValue(),
+                                        gr.getInt16("warningTemp"),
+                                        gr.getInt16("criticalTemp")
                         ));
 
-        ServiceFactory<?, ClassDefinition> classDefinitionServiceFactory =
-                ServiceFactories.sharedService(ctx -> MachineStatusSummary.CLASS_DEFINITION);
+        // now look up the current average temperature and append that to the input event
+        // returns tuple (requestId, serialNum, warningTemp, criticalTemp, averageTemp)
+        StreamStage<Tuple5<String,String, Short, Short, Short>> rawdata =
+                limits.<String, GenericRecord, Tuple5<String, String, Short, Short, Short>>mapUsingIMap(
+                        Names.STATUS_SUMMARY_MAP_NAME,
+                        Tuple4::f1,
+                        (t4, summary) -> Tuple5.tuple5(
+                                t4.f0(),
+                                t4.f1(),
+                                t4.f2(),
+                                t4.f3(),
+                                summary.getInt16("averageBitTemp10s")
+        ));
 
-        results.mapUsingService(classDefinitionServiceFactory, (cdef, event) ->{
-           Tuple2<String,GenericRecord> t2 = Tuple2.tuple2(
-                   event.f0(),
+        // now format the response as a StatusServiceResponse GenericRecord
+        ServiceFactory<?, ClassDefinition> classDefinitionServiceFactory =
+                ServiceFactories.sharedService(ctx -> StatusServiceResponse.CLASS_DEFINITION);
+
+        // package up the results into a GenericRecord of StatusServiceResponse and write to the output map
+        rawdata.mapUsingService(classDefinitionServiceFactory, (cdef, event) ->{
+            String requestId = event.f0();
+            String sn = event.f1();
+            Short warningTemp = event.f2();
+            Short criticalTemp = event.f3();
+            Short averageTemp =  event.f4();
+            String status = categorizeTemp(averageTemp, warningTemp, criticalTemp);
+           return Tuple2.tuple2(
+                   requestId,
                    GenericRecordBuilder.portable(cdef)
-                           .setString("serialNumber", event.f0())
-                           .setInt16("averageBitTemp10s", (short) 0)
-                           .setInt16("warningTemp", event.f1())
-                           .setInt16("criticalTemp", event.f2())
-                           .setString("status", "green").build());
-           return t2;
+                           .setString("serialNumber", sn)
+                           .setInt16("averageBitTemp10s", averageTemp)
+                           .setInt16("warningTemp", warningTemp)
+                           .setInt16("criticalTemp", criticalTemp)
+                           .setString("status", status).build());
         }).writeTo(Sinks.map(outputMapName));
 
         return pipeline;
     }
     public static void main(String []args){
-        Pipeline pipeline = createPipeline("IN", "OUT");
+        if (args.length != 2){
+            System.err.println("Please supply exactly 2 parameters: input map name, output map name");
+            System.exit(1);
+        }
+        Pipeline pipeline = createPipeline(args[0], args[1]);
 
         JobConfig jobConfig = new JobConfig();
         jobConfig.setName("MachineStatusService");
