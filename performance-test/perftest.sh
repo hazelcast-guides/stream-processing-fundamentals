5c9763c1f59a2eabf2ae7afd4ea894674269a032
Randy May <randy.may@hazelcast.com>
2023-09-13 19:06:16 -0400

    updated to 5.1.3, dusted off

--- performance-test/perftest.sh
+++ performance-test/perftest.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 python3 /project/performance-test/retrieve_sns.py
-/home/hazelcast/.local/bin/locust -f /project/performance-test/perftest.py
+locust -f /project/performance-test/perftest.py
