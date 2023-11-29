24053176078901bad2af84a3230769908e0a19b1
Randy May <randy.may@hazelcast.com>
2023-11-29 11:23:00 -0500

    get deploy to viridian working and update the doc accordingly

--- clc-scripts/canceljob.sh
+++ clc-scripts/canceljob.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -e
-clc -c Docker job cancel \
-    `cat ../job/jobname.txt`
+SCRIPTDIR=`dirname $0`
+CLUSTER_NAME=Docker
+if  [ -n $1 ]
+then
+  CLUSTER_NAME=$1
+fi
+
+clc -c $CLUSTER_NAME job cancel `cat $SCRIPTDIR/../job/${CLUSTER_NAME}_job.txt`
